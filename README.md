cd24f21a9d4ead1b3bf4495f22d9418c5c5f0761
th148 <th148@users.noreply.github.com>
2021-03-31 21:58:19 -0700

    Color test now implemented and almost behaves correctly, but not quite. Some of the time it is reversed and I am not sure why that is.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # p04-th148
 Game project
+
+Guides that were helpful:
+
+Pygame Tutorial - Creating Space Invaders
+https://youtu.be/Q-__8Xw9KTM
+
+Snowfall display using Pygame in Python
+https://www.geeksforgeeks.org/snowfall-display-using-pygame-in-python/
+
+
+
--- main.py
+++ main.py
@@ -16,7 +16,14 @@ dark_grey = (50,50,50)
 mid_grey = (150,150,150)
 lite_grey = (200,200,200)
 orange = (255,225,143)
+red = (203,10,10)
+blue = (188,255,188)
+green = (10,10,203)
+yellow = (213,213,0)
+cyan = (0,240,240)
+purple = (203,10,203)
 font_size = 24
+L_font_size = 60
 
 snowing1 = []
 for i in range(40):
@@ -90,17 +97,20 @@ class Destructive_Swing:
         self.large = False
         self.Swing_img = SWING_IMG
         self.mask = pgy.mask.from_surface(self.Swing_img)
+        self.dead = False
 
     def draw(self, surface):
-        surface.blit(self.Swing_img, (self.x, self.y))
+        if self.dead == False:
+            surface.blit(self.Swing_img, (self.x, self.y))
 
     def collision(self, obj):
         obj.dead = True
+        self.dead = True
         return collide(obj, self)
 
 def main():
 
-    running = False
+    not_running = False
     game_over = False
 
     FPS = 60
@@ -109,31 +119,84 @@ def main():
     dialogue_text = ''
 
     font = pgy.font.Font(None, font_size)
+    L_font = pgy.font.Font(None, L_font_size)
     snow1_direction = .1
     snow2_direction = -.05
 
     The_Traitor = Traitor(300, 645)
-    Traitor_attack = Destructive_Swing(300,300)
+    Traitor_attack = Destructive_Swing(330,800)
 
     level = 0
 
     Hellhound_positions = []
     Foliage_positions = []
+    flame_timer = 0
+    color_timer = 0
+    attack_cooldown = 0
 
     Hellhound_direction = -2
 
     Hellhound_quant = 5
     Foliage_quant  = 1
 
-    def sword_swing():
-        Traitor_attack.draw(game_screen)
-        pgy.display.update
+    def color_text_generator(test_text, text_color):
+        color_text = L_font.render(f'{test_text}'
+        , 1 , text_color)
+        game_screen.blit(color_text, (400, 400))
 
-    def game_screen_update(my_text, snow_direct1, snow_direct2):
+    def one_thru_six():
+        random_int = random.randint(1,6)
+        return random_int
+
+    def text_randomizer(randomized_int):
+        random_int = randomized_int
+        if random_int == 1:
+            new_test_text = 'Red'
+        elif random_int == 2:
+            new_test_text = 'Blue'
+        elif random_int == 3:
+            new_test_text = 'Green'
+        elif random_int == 4:
+            new_test_text = 'Yellow'
+        elif random_int == 5:
+            new_test_text = 'Cyan'
+        elif random_int == 6:
+            new_test_text = 'Purple'
+        return new_test_text
+
+    def color_randomizer(randomized_int):
+        random_int2 = randomized_int
+        if random_int2 == 1:
+            new_test_color = red
+        elif random_int2 == 2:
+            new_test_color = blue
+        elif random_int2 == 3:
+            new_test_color = green
+        elif random_int2 == 4:
+            new_test_color = yellow
+        elif random_int2 == 5:
+            new_test_color = cyan
+        elif random_int2 == 6:
+            new_test_color = purple
+        return new_test_color
+
+    def game_screen_update(my_text, snow_direct1, snow_direct2, random_int1, random_int2):
         game_screen.fill(black)
 
         text_label = font.render(f'{my_text}', 1, orange)
         game_screen.blit(text_label, (320,500))
+
+        color_text_generator(text_randomizer(random_int1), color_randomizer(random_int2))
+
+        if flame_timer == 0:
+            Traitor_attack.x = 330
+            Traitor_attack.y = 800
+
+
+        if flame_timer > 0:
+            Traitor_attack.x = 330
+            Traitor_attack.y = 650
+            Traitor_attack.draw(game_screen)
 
         # snow code block vvv
 
@@ -177,17 +240,45 @@ def main():
         if game_over:
             lose_text = font.render("Your fight has ended. Press C to Continue playing or X to eXit."
             , 1 , white)
-            game_screen.blit(lose_text, (300, 400))
+            game_screen.blit(lose_text, (300, 600))
 
 
         pgy.display.update()
 
-    while running == False:
+    while not not_running:
 
         for event in pgy.event.get():
             if event.type == pgy.QUIT:
                 pgy.quit()
                 sys.exit()
+
+        color_timer += 1
+
+        if attack_cooldown > 0:
+            attack_cooldown -= 1
+
+        if color_timer < 60:
+            rand_int1 = 1
+            rand_int2 = 1
+
+        if (color_timer % 60) == 0:
+            rand_int0 = random.randint(1,2)
+            if rand_int0 == 1:
+                rand_int1 = one_thru_six()
+                rand_int2 = one_thru_six()
+                if rand_int1 == rand_int2:
+                    do_match = True
+                elif rand_int1 != rand_int2:
+                    do_match = False
+            elif rand_int0 == 2:
+                matching_int = one_thru_six()
+                rand_int1 = matching_int
+                rand_int2 = matching_int
+                do_match = True
+
+
+        if flame_timer > 0:
+            flame_timer -= 1
 
         if text_timer == 50:
             dialogue_text = 'Hello.'
@@ -204,28 +295,41 @@ def main():
         if text_timer == 500:
             dialogue_text = ''
 
-        text_timer += 1
+        if text_timer < 500:
+            text_timer += 1
 
-                # CONTROLS
+        # CONTROLS
         key_press = pgy.key.get_pressed()
         if key_press[pgy.K_x]:
-            sword_swing()
+            if attack_cooldown == 0:
+                if not do_match:
+                    flame_timer = 14
+                    attack_cooldown += 32
+                elif do_match:
+                    Hellhound_direction -= 1
+                    attack_cooldown += 16
         if key_press[pgy.K_c]:
-            sword_swing()
+            if attack_cooldown == 0:
+                if do_match:
+                    flame_timer = 14
+                    attack_cooldown += 32
+                elif not do_match:
+                    Hellhound_direction -= 1
+                    attack_cooldown += 16
 
-        game_screen_update(dialogue_text, snow1_direction, snow2_direction)
+        game_screen_update(dialogue_text, snow1_direction, snow2_direction, rand_int1, rand_int2)
 
         if len(Hellhound_positions) < 5:
             level += 1
             Hellhound_quant += 2
             Foliage_quant += 1
             for i in range(Hellhound_quant):
-                hellhound = Hellhound(random.randrange(2000, 3000), 645)
+                hellhound = Hellhound(random.randrange(2000, 2600), 645)
                 Hellhound_positions.append(hellhound)
 
         for hellhound in Hellhound_positions[:]:
             hellhound.move(Hellhound_direction)
-            if hellhound.x == 300:
+            if hellhound.x == 340:
                 game_over = True
             if hellhound.collision(Traitor_attack):
                 Hellhound_positions.remove(hellhound)
